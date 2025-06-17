cd13c410f6009bdf0e5070fad2f6870dd908fee3
olegromashkan <oleg.romashkan@outlook.com>
2025-06-17 07:54:18 +0100

    Reapply "Add orders map and improve layout"
    This reverts commit 74091abeeefcdede8d3b556adedc113592d28b07.

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,6 +6,7 @@ import Card from '../components/Card';
 import Modal from '../components/Modal';
 import Calendar from '../components/Calendar';
 import SummaryFeed from '../components/SummaryFeed';
+import OrdersMap from '../components/OrdersMap';
 import Skeleton from '../components/Skeleton';
 import Icon from '../components/Icon';
 import HeatMap from '../components/HeatMap';
@@ -140,20 +141,17 @@ export default function Home() {
         </div>
       </motion.div>
 
-      <div className="flex flex-col lg:flex-row gap-6 flex-1">
-        <div className="lg:w-4/12 h-64 lg:h-full">
-          <HeatMap points={homeData?.points || []} />
+      <div className="flex flex-col md:flex-row gap-6 flex-1 min-h-0">
+        <div className="md:w-60 flex-none">
+          <OrdersMap />
         </div>
-        <div className="lg:flex-1 flex flex-col gap-6">
-          <div className="flex flex-col md:flex-row gap-6 flex-1">
-            <div className="flex-[3] min-w-full md:min-w-[800px]">
-              <Calendar />
-            </div>
-            <div className="flex-[1] w-full md:w-48 h-full">
-              <SummaryFeed />
-            </div>
+        <div className="flex flex-col md:flex-row gap-6 flex-1 min-h-0">
+          <div className="flex-[3] min-w-full md:min-w-[800px] min-h-0">
+            <Calendar />
           </div>
-          <PaymentTypeBar data={homeData?.paymentCounts || {}} />
+          <div className="flex-[1] w-full md:w-48 min-h-0 h-full">
+            <SummaryFeed />
+          </div>
         </div>
       </div>
 
