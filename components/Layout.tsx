8058e30c41d26555fd1efc97cf9429fed1b81f1a
Harry Hoffmann <68178716+olegromashkan@users.noreply.github.com>
2025-06-19 16:05:54 +0300

    feat: revert to sidebar navbar layout

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,4 +1,4 @@
-import { ReactNode } from 'react';
+import { ReactNode, useState } from 'react';
 import Head from 'next/head';
 import Navbar from './Navbar';
 import ToastNotifications from './ToastNotifications';
@@ -11,15 +11,19 @@ interface LayoutProps {
   fullWidth?: boolean;
 }
 export default function Layout({ children, title, fullWidth }: LayoutProps) {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(true);
+
   return (
     <>
       <Head>
         <title>{title ? `${title} | Proovia Report` : 'Proovia Report'}</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
-      <div className="min-h-screen flex flex-col bg-gradient-to-br from-gray-50 to-gray-100 dark:from-gray-900 dark:to-gray-800 text-base-content">
-        <Navbar />
-        <div className="flex-1 flex flex-col min-h-0">
+      <div className="min-h-screen flex bg-gradient-to-br from-gray-50 to-gray-100 dark:from-gray-900 dark:to-gray-800 text-base-content">
+        <Navbar isSidebarOpen={isSidebarOpen} setIsSidebarOpen={setIsSidebarOpen} />
+        <div
+          className={`flex-1 flex flex-col min-h-0 transition-all duration-300 ${isSidebarOpen ? 'lg:ml-60' : 'lg:ml-0'}`}
+        >
           <div className="fixed top-4 right-4 z-40 flex items-center gap-2">
             <NotificationCenter />
             <UserMenu />
@@ -34,7 +38,6 @@ export default function Layout({ children, title, fullWidth }: LayoutProps) {
           >
             {children}
           </main>
-
         </div>
       </div>
     </>
